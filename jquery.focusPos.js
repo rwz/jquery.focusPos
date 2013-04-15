b1e613ee7a14b4a52aee5f3d9b21a43eae62aeeb
Pavel Pravosud <pavel@pravosud.com>
2013-04-15 18:41:26 +0700

    Manually trigger focus on elements before selection

--- jquery.focusPos.js
+++ jquery.focusPos.js
@@ -1,11 +1,11 @@
-/**
+/*
   Focuses the editable element on given position. It's also possible to make
   a selection by providing two positions in most browsers.
 
   Pavel Pravosud <pavel@pravosud.com>
   Licensed under MIT license in 2013
   https://github.com/rwz/jquery.focusPos
- */
+*/
 
 
  (function($){
@@ -32,12 +32,17 @@
     method = function(){
       if (!this.length) return this;
       var element = this.get(0);
+      element.focus()
       element.setSelectionRange.apply(element, getPosition(this, arguments));
       return this;
     }
   else if ('createTextRange' in textarea)
     method = function(){
-      var range = this.get(0).createTextRange(), pos = getPosition(this, arguments);
+      var element = this.get(0),
+        range = element.createTextRange(),
+        pos = getPosition(this, arguments);
+
+      element.focus()
       range.collapse(true);
       range.moveEnd('character', pos[0]);
       range.moveStart('character', pos[1]);
